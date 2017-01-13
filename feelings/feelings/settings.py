af94ab2432494b8d64226204ccbad09f2b3997f2
Kenneth Love <kennethlove@gmail.com>
2017-01-13 10:52:28 -0800

    Basic Thoughts app with admin

--- feelings/feelings/settings.py
+++ feelings/feelings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'thoughts',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
---
+++ feelings/thoughts/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+CONDITIONS = (
+    (0, 'Ecstatic'),
+    (5, 'Passionate'),
+    (10, 'Happy'),
+    (15, 'Optimistic'),
+    (20, 'Content'),
+    (25, 'Bored'),
+    (26, 'Tired'),
+    (27, 'Hungry'),
+    (30, 'Pessimistic'),
+    (35, 'Frustrated'),
+    (40, 'Overwhelmed'),
+    (45, 'Disappointed'),
+    (50, 'Worried'),
+    (55, 'Angry'),
+    (60, 'Jealous'),
+    (65, 'Insecure'),
+    (70, 'Guilty'),
+    (75, 'Fear'),
+    (80, 'Grief'),
+    (85, 'Despair'),
+    (90, 'Paranoid'),
+)
+
+
+class Thought(models.Model):
+    user = models.ForeignKey(User, related_name='thoughts')
+    recorded_at = models.DateTimeField(default=timezone.now, editable=False)
+    condition = models.IntegerField(choices=CONDITIONS)
+    notes = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return '{}: {}'.format(self.recorded_at.strftime('%Y-%m-%d %H:%M:%S'), self.get_condition_display())
+
