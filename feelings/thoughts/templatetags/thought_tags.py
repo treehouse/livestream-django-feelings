235a9f9bc44cf43feeee091e5daa0d64de6b43b9
Kenneth Love <kenneth@gigantuan.net>
2017-01-20 11:38:05 -0800

    Graphs!

--- feelings/thoughts/templatetags/thought_tags.py
+++ feelings/thoughts/templatetags/thought_tags.py
@@ -1,4 +1,8 @@
+import datetime
+import json
+
 from django import template
+from django.utils import timezone
 
 from ..forms import ThoughtForm
 
@@ -9,3 +13,15 @@ register = template.Library()
 def thought_form():
     form = ThoughtForm()
     return {'form': form}
+
+
+@register.simple_tag(takes_context=True)
+def chart_data(context):
+    user = context['user']
+    ten_days_ago = timezone.now() - datetime.timedelta(days=10)
+    thoughts = user.thoughts.filter(recorded_at__gte=ten_days_ago)
+    return json.dumps({
+        'labels': [thought.recorded_at.strftime('%Y-%m-%d') for
+                   thought in thoughts],
+        'series': [[thought.condition for thought in thoughts]]
+    })
