8bd9294b54bc1f82bb8be854f0c6d41773619f3f
Kenneth Love <kenneth@gigantuan.net>
2017-01-20 11:54:04 -0800

    Company and Family models and admin registration

--- feelings/users/models.py
+++ feelings/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Group(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.ForeignKey(User, related_name='%(class)s_created')
+    name = models.CharField(max_length=255)
+    description = models.TextField(default='')
+
+    class Meta:
+        abstract = True
+
+
+class Family(Group):
+    members = models.ManyToManyField(User, related_name='families')
+
+    class Meta:
+        verbose_name_plural = 'families'
+
+
+class Company(Group):
+    members = models.ManyToManyField(User, related_name='companies')
+
+    class Meta:
+        verbose_name_plural = 'companies'
