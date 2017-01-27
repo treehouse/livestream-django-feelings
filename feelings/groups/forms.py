ddfe84687a2417a0525b33c698bb122ab41ead5e
Kenneth Love <kenneth@gigantuan.net>
2017-01-27 12:03:58 -0800

    Invites can now be created, can't be accepted. References #6

--- feelings/groups/forms.py
+++ feelings/groups/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.db.models import Q
+from django.contrib.auth.models import User
 
 from . import models
 
@@ -13,3 +15,18 @@ class FamilyForm(forms.ModelForm):
     class Meta:
         fields = ('name', 'description')
         model = models.Family
+
+
+class CompanyInviteForm(forms.Form):
+    email_or_username = forms.CharField(label="Email/Username")
+
+    def clean_email_or_username(self):
+        data = self.cleaned_data['email_or_username']
+        try:
+            self.invitee = models.User.objects.get(
+                Q(email=data)|Q(username=data)
+            )
+        except models.User.DoesNotExist:
+            raise ValidationError('No such user')
+        return data
+
--- feelings/groups/views/company.py
+++ feelings/groups/views/company.py
@@ -5,6 +5,7 @@ from django.views import generic
 from braces.views import SetHeadlineMixin
 
 from .. import forms
+from .. import models
 
 
 class Create(LoginRequiredMixin, SetHeadlineMixin, generic.CreateView):
@@ -34,9 +35,36 @@ class Update(LoginRequiredMixin, SetHeadlineMixin, generic.UpdateView):
         return reverse('groups:companies:detail', kwargs={
             'slug': self.object.slug})
 
-class Detail(LoginRequiredMixin, generic.DetailView):
+
+class Detail(LoginRequiredMixin, generic.FormView):
+    form_class = forms.CompanyInviteForm
     template_name = 'companies/detail.html'
+
+    def get_success_url(self):
+        self.get_object()
+        return reverse('groups:companies:detail', kwargs={
+            'slug': self.object.slug})
 
     def get_queryset(self):
         return self.request.user.companies.all()
+
+    def get_object(self):
+        self.object = self.request.user.companies.get(
+            slug=self.kwargs.get('slug')
+        )
+        return self.object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object'] = self.get_object()
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        models.CompanyInvite.objects.create(
+            from_user=self.request.user,
+            to_user=form.invitee,
+            company=self.get_object()
+        )
+        return response
 
