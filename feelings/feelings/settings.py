fa0934482d58cac286e2b2fce964b4a137474b35
Kenneth Love <kenneth@gigantuan.net>
2017-03-23 14:37:34 -0700

    static root

--- feelings/feelings/settings.py
+++ feelings/feelings/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
 
